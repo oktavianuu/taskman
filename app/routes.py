b49eebbbb36aa669afb7afb2cc95737a084c0beb
oktavianuu <oktavianuss.misro@gmail.com>
2024-05-24 19:52:20 +0700

    first commit

---
+++ app/routes.py
@@ -0,0 +1,36 @@
+from flask import current_app as app, render_template, request, redirect, url_for
+from .. import db
+from .models import Task
+
+@app.route('/')
+def index():
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_task():
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        new_task = Task(title=title, description=description)
+        db.session.add(new_task)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('add_task.html')
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update_task(id):
+    task = Task.query.get_or_404(id)
+    if request.method == 'POST':
+        task.title = request.form['title']
+        task.description = request.form['description']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('update_task.html', task=task)
+
+@app.route('/delete/<int:id>')
+def delete_task(id):
+    task = Task.query.get_or_404(id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('index'))
